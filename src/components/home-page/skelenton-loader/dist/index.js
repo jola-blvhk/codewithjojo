62e6eed29db21f761e0a13e9bbb746a9bc041c32
jola-blvhk <awujoolabello@gmail.com>
2025-03-30 23:42:42 +0100

    added skeleton loader

--- src/components/home-page/skelenton-loader/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var SkeletonLoader = function () {
-    return (React.createElement("div", { className: "animate-pulse space-y-4" },
-        React.createElement("div", { className: "h-6 w-3/4 bg-gray-300 rounded-md" }),
-        React.createElement("div", { className: "h-6 w-1/2 bg-gray-300 rounded-md" }),
-        React.createElement("div", { className: "h-40 w-full bg-gray-300 rounded-md" })));
-};
-exports["default"] = SkeletonLoader;
